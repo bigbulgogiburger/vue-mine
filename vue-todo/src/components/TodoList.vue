8c9c8132ddc3d9d32d14b117830b4ffcbf0bafe6
bigbulgogiburger <dohoon321@gmail.com>
2023-08-25 10:04:12 +0900

    list까지 완료

---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <ul>
+      <li
+        v-for="(todoItem, index) in todoItems"
+        v-bind:key="todoItem.item"
+        class="shadow"
+      >
+        <i
+          class="checkBtn fa fa-check"
+          v-on:click="toggleComplete(todoItem, index)"
+          v-bind:class="{ checkedButtonCompleted: todoItem.completed }"
+        ></i>
+        <span v-bind:class="{ textCompleted: todoItem.completed }">{{
+          todoItem.item
+        }}</span>
+        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+          <i class="fa fa-trash"></i>
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      todoItems: []
+    };
+  },
+  methods: {
+    removeTodo: function(todoItem, index) {
+      console.log("todoItem");
+      localStorage.removeItem(todoItem.item);
+      this.todoItems.splice(index, 1);
+    },
+    toggleComplete: function(todoItem, index) {
+      console.log("moving");
+      todoItem.completed = !todoItem.completed;
+
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    }
+  },
+  // created는 인스턴스가 생성되자마자 실행되는 라이프사이클 훅
+  created: function() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+          this.todoItems.push(
+            JSON.parse(localStorage.getItem(localStorage.key(i)))
+          );
+          //   this.todoItems.push(localStorage.key(i));
+        }
+
+        // console.log(localStorage.key(i));
+      }
+    }
+  }
+};
+</script>
+
+<style>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 55px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnComplete {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+</style>
