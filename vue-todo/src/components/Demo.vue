00f80e66f76f62559e8d2f42e83647e212d6a20b
bigbulgogiburger <dohoon321@gmail.com>
2023-09-01 18:38:26 +0900

    add demo

---
+++ vue-todo/src/components/Demo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="root">
+    <p>{{ orginalPrice }}</p>
+    <p>{{ doublePrice }}</p>
+    <p>{{ triplePrice }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    orginalPrice() {
+      return this.$store.getters.orginalPrice;
+    },
+    doublePrice() {
+      return this.$store.getters.doublePrice;
+    },
+    triplePrice() {
+      return this.$store.getters.triplePrice;
+    }
+  }
+};
+</script>
+
+<style></style>
